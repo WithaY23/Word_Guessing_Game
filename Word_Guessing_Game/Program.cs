beb874f060607599f426e71486d69e378f10f647
WithaY23 <aebudhoo@aol.com>
2023-07-26 19:47:39 -0400

    Added note to self on several sections and modified strings to be formatted on input

--- Word_Guessing_Game/Program.cs
+++ Word_Guessing_Game/Program.cs
@@ -9,22 +9,25 @@
 
 
 //load data from list for a word
+//Note to self: Create dictionary add method that adds a text file to dictionary based on user input
 List<String> dictionary = new List<String>();
 using (StreamReader file = new StreamReader(@"C:\Users\aebud\source\repos\Word_Guessing_Game\Word_Guessing_Game\Candy.txt"))
 {
+    //Read data till file ends and add it to the dictionary, convert it so that the info has no spaces and is lowercased
+    //DO NOT CHANGE FILES DIRECTLY FOR EFFICIENCY: Accounts for new text files to be formatted with uppercase and spaces
     while (!file.EndOfStream)
     {
-        dictionary.Add((file.ReadLine()));
+        dictionary.Add((file.ReadLine().ToLower().Replace(" ", "")));
     }
 }
 
+//Note to self: Could create random method for beginning, not necessary
     //set up randomness
     Random rnd = new Random();
 int indexList = rnd.Next(1, dictionary.Count());
 
-
+//Note to self: Constructor?
 //set up variables for loop. user's word, program's word, bool for loop, number of guesses
-
 Console.WriteLine(indexList);
 String answer = dictionary[indexList];
 String? userInput = "";
@@ -32,20 +35,21 @@ bool correct =  false;
 int guesses = 0;
 
 
+//Note to self: While method, loop the method in Program.cs or method itself, either works
 //user takes guesses for as long as they don't get the answer
-while (!correct)
+do
 {
 
-    //get input from user
+    //get input from user, format it so that the string is lowercased and has no spaces
     Console.WriteLine("Guess a Word: ");
-    userInput = Console.ReadLine().ToLower();
+    userInput = (Console.ReadLine().ToLower()).Replace(" ", "");
 
 
 
-    if(userInput ==  answer)
+    if (userInput == answer)
     {
         Console.WriteLine("You guessed correctly, congrats!");
-
+        correct = true;
     }
 
 
@@ -54,7 +58,7 @@ while (!correct)
 
 
         //their answer was "higher", tell them that
-        if (String.Compare(userInput, answer) >0 )
+        if (String.Compare(userInput, answer) > 0)
         {
             Console.WriteLine("Too far, try earlier in the alphabet.");
             guesses++;
@@ -75,5 +79,5 @@ while (!correct)
 
 
 
-}
+}while (!correct);
 
